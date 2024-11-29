014f210d2aa19f1c5dc699f8c118f7297f1e4187
Alex P <a117a117a@gmail.com>
2024-11-29 12:55:39 +0100

    completed 2-party-time task

--- week2/practice-exercises/2-party-time/script.js
+++ week2/practice-exercises/2-party-time/script.js
@@ -8,9 +8,36 @@
  * Hints:
  * - make sure to use the correct headers and http method in the request
  */
+import fetch from 'node-fetch';
 
-function makeReservation() {
-  // YOUR CODE GOES IN HERE
+async function makeReservation() {
+  try {
+    const url = 'https://reservation100-sandbox.mxapps.io/rest-doc/api/reservations';
+
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    const body = JSON.stringify({
+      name: 'Alex P',
+      partySize: 4,  
+    });
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: body,
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log('Reservation response:', data);
+
+  } catch (error) {
+    console.error('Error making reservation:', error.message);
+  }
 }
 
 makeReservation();
